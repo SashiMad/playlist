3d1f245713793b4d305564b379e641ee5d0cfd89
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-07-21 16:44:46 +0200

    added thumbnail placeholder and like button, styled them

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,20 @@ function App() {
     <div className="App">
       <ul>
         <li>
+          <button className="thumbnail"></button>
+
           <h3>Lorem ipsum</h3>
           <h4>Lorem ipsum</h4>
 
           <button className="button-play"></button>
+          <button className="button-save"></button>
         </li>
         <li>
+          <button className="thumbnail"></button>
           <h3>Lorem ipsum</h3>
           <h4>Lorem ipsum</h4>
           <button className="button-pause"></button>
+          <button className="button-saved"></button>
         </li>
         <li>
           <h3>Lorem ipsum</h3>
