24203f1a4c0ab68b510587e22ed354068148b536
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-07-21 13:38:59 +0200

    hardcoded song list, added basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import Song from "./Song";
 
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ul>
+        <li>
+          <h3>Lorem ipsum</h3>
+          <h4>Lorem ipsum</h4>
+        </li>
+        <li>
+          <h3>Lorem ipsum</h3>
+          <h4>Lorem ipsum</h4>
+        </li>
+        <li>
+          <h3>Lorem ipsum</h3>
+          <h4>Lorem ipsum</h4>
+        </li>
+        <li>
+          <h3>Lorem ipsum</h3>
+          <h4>Lorem ipsum</h4>
+        </li>
+        <li>
+          <h3>Lorem ipsum</h3>
+          <h4>Lorem ipsum</h4>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Song.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Song() {
+  return (
+    <div>
+      <button>Thumbnail</button>
+
+      <h3>Lorem ipsum</h3>
+      <h2>Lorem ipsum</h2>
+    </div>
+  );
+}
